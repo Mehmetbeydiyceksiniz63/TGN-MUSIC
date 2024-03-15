e141be897794ca30e7d7ddb0cac8131ffc88954e
Mehmetbeydiyceksiniz63 <149189769+Mehmetbeydiyceksiniz63@users.noreply.github.com>
2024-03-15 22:48:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Indianbotupdates")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheGodfatherChat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mehmetttbio")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/hissizmehmet")
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
@@ -83,21 +83,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
+    "START_IMG_URL", "https://te.legra.ph/musickss-03-12"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/b55344a1bb8076f923183.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/musickss-03-12"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/musickss-03-12"
+STATS_IMG_URL = "https://te.legra.ph/musickss-03-12"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/musickss-03-12"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/musickss-03-12"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/musickss-03-12"
+YOUTUBE_IMG_URL = "https://te.legra.ph/musickss-03-12"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/musickss-03-12"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/musickss-03-12"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/musickss-03-12"
 
 
 def time_to_seconds(time):
